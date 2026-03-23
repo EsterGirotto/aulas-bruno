0f521c6df189896c4d4a24f90eea3e974410e79e
EsterGirotto <estergirotto775@gmail.com>
2026-03-23 13:53:02 -0300

    testando

--- funcoes.php
+++ funcoes.php
@@ -11,15 +11,10 @@ function dividir ($n1, $n2){
     return ($n1 / $n2);
 }
 
-<<<<<<< HEAD
 function raiz ($num1, $num2){
-    return $num1 ^ (1/2); a
+    return $num1 ^ (1/2); 
 }
 
-=======
-function Raiz($num1)
-{
-    return $num1 ^ (1/2);
-}
->>>>>>> 2b1df04c07087578affee9ac67e576626f3849de
+echo"Valorant";
+
 ?>
